7030011b5de9eec0ed628674d41314f2d85ea955
Esneider <esneider.m97@gmail.com>
2021-03-29 11:37:47 -0500

    deleting unused code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,13 +8,5 @@ export const environment = {
     messagingSenderId: '610775376734',
     appId: '1:610775376734:web:dd54a289e351343952e5cf',
     measurementId: 'G-DMF1GCZZRF'
-
-    // apiKey: 'AIzaSyCdglZuhfBrTlvs4YdN3XXJDP6nU1KpllA',
-    // authDomain: 'control-clientes-e756d.firebaseapp.com',
-    // projectId: 'control-clientes-e756d',
-    // storageBucket: 'control-clientes-e756d.appspot.com',
-    // messagingSenderId: '1035083959346',
-    // appId: '1:1035083959346:web:c02ab9883d84d4af3a802b',
-    // measurementId: 'G-6X6W0SHVCV'
   }
 };
